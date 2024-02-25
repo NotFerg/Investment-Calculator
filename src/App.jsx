cab89faae93d4bed10e1eda39b3d65d36fafe32a
Fergus <AFR0155@DLSUD.EDU.PH>
2024-02-25 11:47:08 +0800

    Investment Application finished

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,36 @@
 import Header from "./components/Header/Header";
 import UserInput from "./components/UserInput/UserInput";
+import Results from "./components/Results/Results";
+import { useState } from "react";
 
 function App() {
+  const [userInput, setUserInput] = useState({
+    initialInvestment: 10000,
+    annualInvestment: 1200,
+    expectedReturn: 6,
+    duration: 10,
+  });
+
+  const inputIsValid = userInput >= 1;
+
+  function handleChange(inputIdentifier, newValue) {
+    setUserInput((prevUserInput) => {
+      return {
+        ...prevUserInput,
+        [inputIdentifier]: +newValue,
+      };
+    });
+  }
+
   return (
     <>
       <Header />
-      <UserInput />
+      <UserInput userInput={userInput} onChange={handleChange} />
+      {inputIsValid ? (
+        <p className="center">Please Enter A Duration Greater Than 0 </p>
+      ) : (
+        <Results input={userInput} />
+      )}
     </>
   );
 }
